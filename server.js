14734d60fc04fe7cf22848b329e6027614e932f2
Edward Harley <edharley19@gmail.com>
2020-03-15 17:23:08 -0400

    Feat: server set up on localhost

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const app = express();
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on PORT ${PORT}`);
+});
