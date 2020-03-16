b8a26f51515ee07fd3661df9357b5403d70e9f37
Edward Harley <edharley19@gmail.com>
2020-03-15 22:16:58 -0400

    Feat: Article model added

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const morgan = require("morgan");
-const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const axios = require("axios");
 const cheerio = require("cheerio");
 const app = express();
 
 app.use(morgan("dev"));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+const db = require("./models");
+mongoose.connect("mongodb://localhost/scraper", { useNewUrlParser: true });
 
 const PORT = process.env.PORT || 3000;
 
